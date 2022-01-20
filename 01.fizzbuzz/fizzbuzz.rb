fd53855e3b3ba4ee88baf10e06b44c897dec29ac
caminotak <takjiroy@yahoo.co.jp>
2022-01-21 05:40:02 +0800

    fizzbuzz課題 訂正1

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,16 +1,13 @@
-number =0
+number = 0
 while number < 20
   number += 1
- if number%15 ==0
-     puts "FizzBuzz"
- else   
-     case
-     when number%3 == 0
-         puts "Fizz"
-     when number%5 == 0 
-         puts "Buzz"
-     else
-         puts number
-     end
- end
-end 
+  if number % 15 == 0
+    puts "FizzBuzz"
+  elsif number % 3 == 0
+    puts "Fizz"
+  elsif number % 5 == 0
+    puts "Buzz"
+  else
+    puts number
+  end
+end
