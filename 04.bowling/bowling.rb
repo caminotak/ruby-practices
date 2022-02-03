a06e31476f0a16b0ef773f99bc5f31cb5e0fff22
caminotak <takjiroy@yahoo.co.jp>
2022-02-03 16:04:43 +0800

    5th commit after reset

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,44 +1,43 @@
 #!/usr/bin/env ruby
 
-ScoresWithX = ARGV[0]
-ScoresWithoutX = ScoresWithX.gsub(/X/, '10')
-actual_throws = ScoresWithoutX.split(',')
+scores_with_x = ARGV[0]
+scores_without_x = scores_with_x.gsub(/X/, '10')
+actual_throws = scores_without_x.split(',')
 
 # 1-9
-score_board = []
+frames = []
 9.times do
   if actual_throws[0] == '10'
-    score_board << [10, 0]
+    frames << [10, 0]
     actual_throws.shift
   else
-    score_board << actual_throws.slice!(0, 2).map(&:to_i)
+    frames << actual_throws.shift(2).map(&:to_i)
   end
 end
 
 # 10
-score_board << if actual_throws[0] == '10'
-                 [10, actual_throws[1].to_i, actual_throws[2].to_i]
-               elsif actual_throws[0].to_i + actual_throws[1].to_i == 10
-                 [actual_throws[0].to_i, actual_throws[1].to_i, actual_throws[2].to_i]
-               else
-                 [actual_throws[0].to_i, actual_throws[1].to_i, 0]
-               end
+frames << if !actual_throws[2]
+            actual_throws.map(&:to_i).push(0)
+          else
+            actual_throws.map(&:to_i)
+          end
 
-score_by_frame = [score_board[9][0] + score_board[9][1] + score_board[9][2]]
+scores_by_frame = []
+
 9.times do |n|
-  score_by_frame << if score_board[n][0] == 10 && score_board[n + 1][0] == 10
-                      if n == 8
-                        20 + score_board[n + 1][1]
-                      else
-                        20 + score_board[n + 2][0]
-                      end
-                    elsif score_board[n][0] == 10
-                      10 + score_board[n + 1][0] + score_board[n + 1][1]
-                    elsif score_board[n][0] + score_board[n][1] == 10
-                      10 + score_board[n + 1][0]
-                    else
-                      score_board[n][0] + score_board[n][1]
-                    end
+  scores_by_frame << if frames[n][0] == 10 && frames[n + 1][0] == 10
+                       if n == 8
+                         20 + frames[n + 1][1]
+                       else
+                         20 + frames[n + 2][0]
+                       end
+                     elsif frames[n][0] == 10
+                       10 + frames[n + 1].sum
+                     elsif frames[n][0] + frames[n][1] == 10
+                       10 + frames[n + 1][0]
+                     else
+                       frames[n].sum
+                     end
 end
 
-p score_by_frame.sum
+p scores_by_frame.sum + frames[9].sum
