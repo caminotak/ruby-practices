b7841752227edd19ea3b6383116dc60f2bf724ec
caminotak <takjiroy@yahoo.co.jp>
2022-03-04 16:04:06 +0900

    4th commit リファクタリング

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -3,59 +3,50 @@
 
 require 'optparse'
 
-def word_count(files)
-  option = ARGV.getopts('l')
+@option = ARGV.getopts('l')
+
+# 出力用のメソッド
+def word_count(files, pipeline)
   if files.empty? # 引数がない場合（パイプラインから標準入力）
-    str = readlines.join
-    lines = str.count("\n")
-    if option['l']
-      puts lines.to_s.rjust(8).to_s
-    else
-      words = str.split(/\s+/).size
-      bytes = str.bytesize
-      puts "#{lines.to_s.rjust(8)}#{words.to_s.rjust(8)}#{bytes.to_s.rjust(8)}"
-    end
-  else # 引数がある場合(引数、getsから標準入力)
+    lines = pipeline.count("\n")
+    words = pipeline.split(/\s+/).size
+    bytes = pipeline.bytesize
+    print lines.to_s.rjust(8).to_s
+    print @option['l'] ? "\n" : "#{words.to_s.rjust(8)}#{bytes.to_s.rjust(8)}\n"
 
-    total_lines = 0
-    total_words = 0
-    total_bytes = 0
-
+  else # 引数がある場合(引数 or getsから標準入力)
+    total = { 'lines' => 0, 'words' => 0, 'bytes' => 0 }
     files.each do |filepath|
       str = File.read(filepath)
       lines = str.count("\n")
-      total_lines += lines
-      if option['l']
-        puts "#{lines.to_s.rjust(8)} #{filepath}"
-      else
-        words =  str.split(/\s+/).size
-        bytes =  File.stat(filepath).size
-        puts "#{lines.to_s.rjust(8)}#{words.to_s.rjust(8)}#{bytes.to_s.rjust(8)} #{filepath}"
-        total_words += words
-        total_bytes += bytes
-      end
+      words =  str.split(/\s+/).size
+      bytes =  File.stat(filepath).size
+      print lines.to_s.rjust(8)
+      print @option['l'] ? " #{filepath}\n" : "#{words.to_s.rjust(8)}#{bytes.to_s.rjust(8)} #{filepath}\n"
+      total['lines'] += lines
+      total['words'] += words
+      total['bytes'] += bytes
     end
 
-    if files.length > 1
-      print total_lines.to_s.rjust(8).to_s
-      puts option['l'] ? ' total' : "#{total_words.to_s.rjust(8)}#{total_bytes.to_s.rjust(8)} total"
+    if files.length > 1 # 複数の引数があった場合にtotalを表示する行
+      print total['lines'].to_s.rjust(8).to_s
+      puts @option['l'] ? ' total' : "#{total['words'].to_s.rjust(8)}#{total['bytes'].to_s.rjust(8)} total"
     end
-
   end
 end
 
 if __FILE__ == $PROGRAM_NAME
-  if ARGV.empty?
-    pipeline = readline
-    if !pipeline.empty?# パイプライン入力がある場合
+  if ARGV[0].nil? # 引数がない場合
+    pipeline = readlines.join
+    if !pipeline.empty? # パイプライン入力がある場合
       files = []
-    else # パイプライン記述がない場合、入力待ちになりこの分岐へ進まない
+    else # パイプライン記述がない場合、入力待ちになりこの分岐へ進まない。。。
       puts 'ファイル名を入力して下さい >>'
       input = gets
       files = input.chomp.split
     end
-  else
+  else # 引数がある場合
     files = ARGV
   end
-  word_count(files)
+  word_count(files, pipeline)
 end
