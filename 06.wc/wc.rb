e69d829d93f6ebd7f7ef45aae97d5d318e33f43a
caminotak <takjiroy@yahoo.co.jp>
2022-03-04 12:39:42 +0900

    2nd commit 標準入力追加

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -3,42 +3,59 @@
 
 require 'optparse'
 
-option = ARGV.getopts('l')
-
-if ARGV.empty?
-  str = readlines.join
-  lines = str.lines.count
-  if option['l']
-    puts lines.to_s.rjust(8).to_s
-  else
-    words = str.split(/\s+/).size
-    bytes = str.bytesize
-    puts "#{lines.to_s.rjust(8)}#{words.to_s.rjust(8)}#{bytes.to_s.rjust(8)}"
-  end
-else
-
-  total_lines = 0
-  total_words = 0
-  total_bytes = 0
-
-  ARGV.each do |filepath|
-    str = File.read(filepath)
-    lines = str.lines.count
-    total_lines += lines
+def word_count(files)
+  option = ARGV.getopts('l')
+  if files.empty? # 引数がない場合（パイプラインから標準入力）
+    str = readlines.join
+    lines = str.count("\n")
     if option['l']
-      puts "#{lines.to_s.rjust(8)} #{filepath}"
+      puts lines.to_s.rjust(8).to_s
     else
-      words =  str.split(/\s+/).size
-      bytes =  File.stat(filepath).size
-      puts "#{lines.to_s.rjust(8)}#{words.to_s.rjust(8)}#{bytes.to_s.rjust(8)} #{filepath}"
-      total_words += words
-      total_bytes += bytes
+      words = str.split(/\s+/).size
+      bytes = str.bytesize
+      puts "#{lines.to_s.rjust(8)}#{words.to_s.rjust(8)}#{bytes.to_s.rjust(8)}"
     end
-  end
+  else # 引数がある場合(引数、getsから標準入力)
 
-  if ARGV[1]
-    print total_lines.to_s.rjust(8).to_s
-    puts option['l'] ? ' total' : "#{total_words.to_s.rjust(8)}#{total_bytes.to_s.rjust(8)} total"
-  end
+    total_lines = 0
+    total_words = 0
+    total_bytes = 0
 
+    files.each do |filepath|
+      str = File.read(filepath)
+      lines = str.count("\n")
+      total_lines += lines
+      if option['l']
+        puts "#{lines.to_s.rjust(8)} #{filepath}"
+      else
+        words =  str.split(/\s+/).size
+        bytes =  File.stat(filepath).size
+        puts "#{lines.to_s.rjust(8)}#{words.to_s.rjust(8)}#{bytes.to_s.rjust(8)} #{filepath}"
+        total_words += words
+        total_bytes += bytes
+      end
+    end
+
+    if files[1]
+      print total_lines.to_s.rjust(8).to_s
+      puts option['l'] ? ' total' : "#{total_words.to_s.rjust(8)}#{total_bytes.to_s.rjust(8)} total"
+    end
+
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  if ARGV.empty?
+    pipeline = readline
+    if !pipeline.empty?# パイプライン入力がある場合
+      files = []
+    else # パイプライン記述がない場合、入力待ちになりこの分岐へ進まない
+      puts 'ファイル名を入力して下さい >>'
+      input = gets
+      files = input.chomp.split
+    end
+  else
+    files = ARGV
+  end
+  word_count(files)
 end
